0fbe891153598a5f48887b909c7cab50f2ec9a68
Aaron Bieber <deftly@gmail.com>
2013-07-01 11:00:31 -0600

    browser stuff working

--- browser/sentimental_full.js
+++ browser/sentimental_full.js
@@ -1,13 +1,7 @@
 ;(function(e,t,n){function i(n,s){if(!t[n]){if(!e[n]){var o=typeof require=="function"&&require;if(!s&&o)return o(n,!0);if(r)return r(n,!0);throw new Error("Cannot find module '"+n+"'")}var u=t[n]={exports:{}};e[n][0].call(u.exports,function(t){var r=e[n][1][t];return i(r?r:t)},u,u.exports)}return t[n].exports}var r=typeof require=="function"&&require;for(var s=0;s<n.length;s++)i(n[s]);return i})({1:[function(require,module,exports){
-var analyze = require('./lib/sentimental').analyze,
-    negativity = require('./lib/sentimental').negativity,
-    positivity = require('./lib/sentimental').positivity;
-
-module.exports = {
-  analyze    : analyze,
-  negativity : negativity,
-  positivity : positivity
-};
+window.analyze = require('./lib/sentimental').analyze,
+    window.negativity = require('./lib/sentimental').negativity,
+    window.positivity = require('./lib/sentimental').positivity;
 
 },{"./lib/sentimental":2}],2:[function(require,module,exports){
 var neg5 = require('../wordLists/neglist.js').neg5,
@@ -62,6 +56,7 @@ function negativity (phrase) {
 // -------------------------------------------------- //
 
 function positivity (phrase) {
+	console.log('here');
   var addPush = function(t, score){
     hits += score;
     words.push(t);
---
+++ browser_index.js
@@ -0,0 +1,3 @@
+window.analyze = require('./lib/sentimental').analyze,
+    window.negativity = require('./lib/sentimental').negativity,
+    window.positivity = require('./lib/sentimental').positivity;
